3106450686a6dacfdeca0ecdc867e1ef1e2b71f0
Christopher <bcsmithboys1@gmail.com>
2022-02-05 14:30:50 -0600

    added part 5 to functions

--- 6-Functions.cpp
+++ 6-Functions.cpp
@@ -77,3 +77,17 @@ void oscar_wilde_quote()
 int main() {
     oscar_wilde_quote();
 }
+
+//Part 5
+///////////////////////////
+#include <iostream>
+
+double average(double num1, double num2)
+{
+    return((num1 + num2) / 2);
+}
+
+int main() {
+    std::cout << average(42.0, 24.0) << "\n";
+    std::cout << average(1.0, 2.0) << "\n";
+}
