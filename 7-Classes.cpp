a7d2ff6b09a8b70395d9fe2cb48e6777a5703738
Christopher <bcsmithboys1@gmail.com>
2022-02-12 15:24:11 -0600

    Updated part 1

--- 7-Classes.cpp
+++ 7-Classes.cpp
@@ -1,29 +1,36 @@
-//Part 1///////////////
-///////////////////////////////////////////////////
+
+//Part 2
+/////////////////////////////////
+
 
 //music.cpp
+
 #include <iostream>
 #include "song.hpp"
 
 int main() {
 	Song electric_relaxation;
-	electric_relaxation.add_title("electric relaxation");
-	std::cout << electric_relaxation.get_title();
+	electric_relaxation.add_artist("A Tribe Called Quest");
+	std::cout << electric_relaxation.get_artist();
 }
 
-
 //song.hpp
+
 #include <string>
 
 class Song {
 	std::string title;
+	std::string artist;
+
 public:
 	void add_title(std::string new_title);
 	std::string get_title();
+	void add_artist(std::string new_artist);
+	std::string get_artist();
 };
 
-
 //song.cpp
+
 #include "song.hpp"
 
 void Song::add_title(std::string new_title) {
@@ -31,4 +38,12 @@ void Song::add_title(std::string new_title) {
 }
 std::string Song::get_title() {
 	return title;
+}
+
+
+void Song::add_artist(std::string new_artist) {
+	artist = new_artist;
+}
+std::string Song::get_artist() {
+	return artist;
 }
