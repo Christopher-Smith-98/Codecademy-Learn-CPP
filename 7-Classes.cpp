b07917294d8ae22194bfaa4091c2fde839a41516
Christopher <bcsmithboys1@gmail.com>
2022-02-12 15:47:12 -0600

    Added part 3, constructors

--- 7-Classes.cpp
+++ 7-Classes.cpp
@@ -47,3 +47,44 @@ void Song::add_artist(std::string new_artist) {
 std::string Song::get_artist() {
 	return artist;
 }
+
+//Part 3
+///////////////////
+
+
+//music.cpp
+
+#include <iostream>
+#include "song.hpp"
+
+int main() {
+	Song back_to_black("Back to Black", "Amy Winehouse");
+}
+
+//song.hpp
+
+#include <string>
+
+class Song {
+	const std::string title;
+	const std::string artist;
+
+public:
+	Song(std::string new_title, std::string new_artist)
+		: title(new_title), artist(new_artist) {};
+
+	std::string get_title();
+	std::string get_artist();
+};
+
+//song.cpp
+
+#include "song.hpp"
+
+std::string Song::get_title() {
+	return title;
+}
+
+std::string Song::get_artist() {
+	return artist;
+}
